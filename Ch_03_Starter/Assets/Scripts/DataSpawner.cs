421cd38ac0ceb6ba42c9944de0458f6f61d00f38
Juanse Colina <juansebas_colina@hotmail.com>
2024-06-12 17:36:42 -0400

    prototype/clone pattern

--- Ch_03_Starter/Assets/Scripts/DataSpawner.cs
+++ Ch_03_Starter/Assets/Scripts/DataSpawner.cs
@@ -8,5 +8,19 @@ public class DataSpawner : MonoBehaviour
     {
         Enemy ogre = new Enemy(10, "RAWR", "Ogre");
         ogre.Print();
+        IPrototype clonedPrototype = ogre.ShallowClone();
+
+        if (clonedPrototype is Enemy clonedEnemy)
+        {
+            clonedEnemy.Print();
+            ogre.Name = "Monstrous Ogre";
+            ogre.Damage = 30;
+            ogre.Print();
+            clonedEnemy.Print();
+        }
+        else
+        {
+            Debug.Log("Failed to clone ogre. Cloned object is not an Enemy...");
+        }
     }
 }
--- Ch_03_Starter/Assets/Scripts/Enemy.cs
+++ Ch_03_Starter/Assets/Scripts/Enemy.cs
@@ -1,8 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Enemy 
+public interface IPrototype
+{
+    IPrototype ShallowClone();
+    IPrototype DeepClone();
+}
+
+public class Enemy : IPrototype
 {
     public int Damage;
     public string Message;
@@ -10,13 +15,37 @@ public class Enemy
 
     public Enemy(int dmg, string msg, string name)
     {
-        this.Damage = dmg;
-        this.Message = msg;
-        this.Name = name;
+        Damage = dmg;
+        Message = msg;
+        Name = name;
     }
 
     public void Print()
     {
         Debug.LogFormat($"{Message}! {Name} can hit for {Damage} HP.");
+    }
+
+    public IPrototype ShallowClone()
+    {
+        //1
+        IPrototype newPrototype;
+        //2
+        try
+        {
+            //3
+            newPrototype = (IPrototype) this.MemberwiseClone();
+        }
+        //4
+        catch (Exception e)
+        {
+            Console.WriteLine("Error cloning: " + e);
+            throw;
+        }
+        return newPrototype;
+    }
+
+    public IPrototype DeepClone()
+    {
+        return this;
     }
 }
